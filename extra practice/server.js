8d82ad56bd8e045c5ecde1e2d8a5126867d6dc7b
Azka azeem <azkaazeem804@gmail.com>
2026-06-18 17:34:03 +0500

    learn basic cookie and cookie parser

--- extra practice/server.js
+++ extra practice/server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const app = express();
 const user = require("./routes/user.js");
 const post = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
 
 app.listen("8080", () => {
     console.log("app is listening on port 8080");
 });
 
+app.use(cookieParser());
+
 app.get("/" , (req , res) => {
     res.send("User home page");
+});
+
+app.get("/getcookie" , (req , res) => {
+    res.cookie("greet" , "hello");
+    res.send("send your some cookie");
+});
+
+app.get("/greet" , (req , res) => {
+    let {name = "anonyms"} = req.cookie;
+    res.send(`Hi, ${name}`);
 });
 
 app.use("/user" , user);
