8c80c0c0c37767f5d8104c6712a1681afc47fc35
Azka azeem <azkaazeem804@gmail.com>
2026-06-19 15:02:04 +0500

    learning main function of cookie

--- extra practice/server.js
+++ extra practice/server.js
@@ -2,27 +2,13 @@ const express = require("express");
 const app = express();
 const user = require("./routes/user.js");
 const post = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+app.use(session({secret: "mysecretstring"}));
+app.get("/text" , (req,res) => {
+    res.send("text successful!");
+});
 
 app.listen("8080", () => {
     console.log("app is listening on port 8080");
 });
-
-app.use(cookieParser());
-
-app.get("/" , (req , res) => {
-    res.send("User home page");
-});
-
-app.get("/getcookie" , (req , res) => {
-    res.cookie("greet" , "hello");
-    res.send("send your some cookie");
-});
-
-app.get("/greet" , (req , res) => {
-    let {name = "anonyms"} = req.cookie;
-    res.send(`Hi, ${name}`);
-});
-
-app.use("/user" , user);
-app.use("/posts" , post);
