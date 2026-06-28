9a97ee3cbc95aba9ad5514385a31c19781392d46
Azka azeem <azkaazeem804@gmail.com>
2026-06-28 09:06:14 +0500

    something wrong

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,8 +1,6 @@
 const Listing = require("../models/listing.js");
-const  mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAPTILER_API;
-
-const baseClient = mbxGeocoding({ accessToken: mapToken });
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = process.env.MAPTILER_API;
 
 // LISTINGS CONTROLLERS
 module.exports.index = async (req, res) => {
@@ -25,20 +23,23 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-    let response = await geocodingClient.forwardGeocode({
-        query: req.body.listing.location,
-        limit: 1,
-    }).send();
+    let response = await maptilerClient.geocoding.forward(req.body.listing.location, { limit: 1 });
 
-    let url = req.file ? req.file.path : "";
-    let filename = req.file ? req.file.filename : "";
+    const url = req.file.path;
+    const filename = req.file.filename;
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = { url , filename };
-    await newListing.save();
-    req.flash("success", "New Listing created successfully");
+    newListing.image = { url, filename };
+    
+    newListing.geometry = response.features[0].geometry;
+
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
-}
+};
 
 module.exports.editListing = async (req, res) => {
     let { id } = req.params;
--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,15 @@
+maptilersdk.config.apiKey = mapToken; // mapToken ejs file se aa raha hoga
 
-
-const key = mapToken;
-
-const map = new maplibregl.Map({
-  container: 'map', // container id
-  style: `https://api.maptiler.com/maps/streets-v4/style.json?key=${key}`, // style URL
-  center: [73.0479, 33.6844], // starting position [lng, lat] (Islamabad, PK)
-  zoom: 9 // starting zoom
+const map = new maptilersdk.Map({
+  container: 'map', // Jis div mein map dikhana hai uska ID
+  style: maptilersdk.MapStyle.STREETS,
+  center: listing.geometry.coordinates, // Starting position [lng, lat]
+  zoom: 9 // Starting zoom level
 });
+
+// Map par marker lagane ke liye
+const marker = new maptilersdk.Marker({ color: "#fe424d" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(new maptilersdk.Popup({offset: 25})
+  .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
+  .addTo(map);
